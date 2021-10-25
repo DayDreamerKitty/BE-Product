5dff46aeb859517d761fee92dac067c80d9e67e2
Kitty <zainab_97@hotmail.co.uk>
2021-10-25 16:29:23 +0300

    partial

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ app.get("/api/products", (req, res) => {
   res.json(data);
 });
 
+app.post("/api/products", (req, res) => {
+  res.json("response");
+});
+
+app.get("/api/products/:productId", (req, res) => {
+  const products = data.find((product) => product.id === +req.params.productId);
+  res.json(products);
+});
+
+app.delete("/api/products/:productId", (req, res) => {
+  const products = data.find((product) => product.id === +productId);
+  res.json(products);
+});
 app.listen(8000, () => console.log("Hello"));
