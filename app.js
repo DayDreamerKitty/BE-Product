48602d3bb94f838754e9b41da8b88ba2cab7a861
Kitty <zainab_97@hotmail.co.uk>
2021-10-25 17:08:48 +0300

    solved

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require("express");
+let products = require("./data");
 const data = require("./data");
-
 const app = express();
+
+app.use(express.json);
 
 app.get("/api/products", (req, res) => {
   res.json(data);
 });
 
 app.post("/api/products", (req, res) => {
-  res.json("response");
-});
-
-app.get("/api/products/:productId", (req, res) => {
-  const products = data.find((product) => product.id === +req.params.productId);
-  res.json(products);
+  products.push(req.body);
+  res.json(req.body);
 });
 
 app.delete("/api/products/:productId", (req, res) => {
-  const products = data.find((product) => product.id === +productId);
+  const productId = req.params.productId;
+  const product = data.find((product) => product.id === +productId);
   res.json(products);
+  if (product) {
+    products = products.filter((product) => product.id !== +productId);
+    res.status(204);
+    return res.end();
+  } else {
+    return res.status(404).json({ message: "Error" });
+  }
 });
+
 app.listen(8000, () => console.log("Hello"));
