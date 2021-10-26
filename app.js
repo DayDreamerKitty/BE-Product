2fb4d414157b0486119602d1455bb9b26a5cf9cc
Kitty <zainab_97@hotmail.co.uk>
2021-10-26 16:06:01 +0300

    mongoose

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { connect } = require("./apis/products/routes");
 const productRoutes = require("./apis/products/routes");
 
 const app = express();
@@ -6,4 +7,5 @@ const app = express();
 app.use(express.json);
 app.use("/api/products", productRoutes);
 
+connectDB();
 app.listen(8000, () => console.log("Hello"));
